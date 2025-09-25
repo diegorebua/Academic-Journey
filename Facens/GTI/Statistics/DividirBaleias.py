4dd26766ac3c849c84664bc6ac7ca029983fdf40
Diego Rebuá <diegorebua@gmail.com>
2025-09-25 09:45:22 -0300

    Task update

--- Facens/GTI/Statistics/DividirBaleias.py
+++
@@ -1,3 +0,0 @@
-baleia = 6
-modulo_baleias = baleia // 3
-print("Divisão de baleias =", modulo_baleias)
--- Facens/GTI/Statistics/SomaMamiferos.py
+++
@@ -1,8 +0,0 @@
-gato = 6
-cachorro = 10
-urso = 10
-tartaruga = 12
-baleia = 6
-
-mamiferos = gato + cachorro + urso
-print("mamiferos =", mamiferos)
