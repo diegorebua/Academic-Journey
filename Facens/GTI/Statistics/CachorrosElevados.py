8b293bd84f9a2f7b1d533023c950b3b55d8cefe1
Diego Rebuá <diegorebua@gmail.com>
2025-11-03 21:53:01 -0300

    feat: add

--- Facens/GTI/Statistics/CachorrosElevados.py
+++
@@ -1,3 +0,0 @@
-cachorro = 10
-cachorros_quadrado = cachorro ** 2
-print("cachorros_quadrado =", cachorros_quadrado)
--- Facens/GTI/Statistics/RaizQuadrada.py
+++
@@ -1,5 +0,0 @@
-import math
-
-tartaruga = 12
-raiz_tartarugas = math.sqrt(tartaruga)
-print("Raiz de tartarugas =", raiz_tartarugas)
